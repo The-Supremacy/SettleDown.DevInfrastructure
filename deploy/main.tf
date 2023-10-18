553ac0ad3f8200bbf7c35c28c9a6c72e926ed722
oxface <r.g.klapchuk@gmail.com>
2023-10-18 18:39:52 +0300

    Formatting.

--- deploy/main.tf
+++ deploy/main.tf
@@ -2,7 +2,7 @@ module "acr" {
   source             = "github.com/The-Supremacy/SettleDown.InfrastructureModules//container_registry"
   azure_cicd_sp_name = var.azure_cicd_sp_name
   tags = {
-    Area = "Dev infrastructure"
+    Area        = "Dev infrastructure"
     Environment = "Shared"
   }
 }
